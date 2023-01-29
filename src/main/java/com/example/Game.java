7d53977468b647b4ca1a8c2cb4b63da9b0a6ccb3
williamDenStore <william.karlsson1999@gmail.com>
2023-01-29 14:44:16 +0100

    refactor in Game.java

--- src/main/java/com/example/Game.java
+++ src/main/java/com/example/Game.java
@@ -1,36 +1,55 @@
 package com.example;
 
 public class Game {
-    int rollScore;
-    int bonus;
-    int lastRoll;
-    double frames;
-    public void roll(int i) {
+    private int totalScore;
+    private int bonus;
+    private int lastRoll;
+    private double frames;
+    public void roll(int rollScore) {
         if (!gameIsOver()) {
-            if (bonus > 0) {
-                rollScore += i;
-                if (bonus > 2) {
-                    rollScore += i;
-                    bonus--;
-                }
-                bonus--;
-            }
-            if (i == 10 && frames<10) {
-                bonus += 2;
-                frames += 0.5;
-            }
-            if (i + lastRoll == 10 && i != 10)
-                bonus += 1;
-            lastRoll = i;
-            rollScore += i;
-            frames += 0.5;
+            if (bonus > 0)
+                applyBonus(rollScore);
+            addBonus(rollScore);
+            updateVariables(rollScore);
             if(frames>=10.5)
-                rollScore-=i;
+                totalScore -=rollScore;
         }
     }
 
+    private void updateVariables(int i) {
+        lastRoll = i;
+        totalScore += i;
+        frames += 0.5;
+    }
+
+    private void addBonus(int i) {
+        if (strike(i)) {
+            bonus += 2;
+            frames += 0.5;
+        }
+        if (spare(i))
+            bonus += 1;
+    }
+
+    private boolean strike(int i) {
+        return i == 10 && frames < 10;
+    }
+
+    private boolean spare(int i) {
+        return i + lastRoll == 10 && i != 10;
+    }
+
+    private void applyBonus(int i) {
+        totalScore += i;
+        if (bonus > 2) {
+            totalScore += i;
+            bonus--;
+        }
+        bonus--;
+    }
+
     public int score() {
-        return rollScore;
+        return totalScore;
     }
 
     public boolean gameIsOver() {
